fc48d1d4ff8eec6d7fec73e686ae620460ba435a
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-06-07 03:24:46 +0545

    config: style issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: ['bg-opacity-30'],
   theme: {
     extend: {
       backgroundImage: {
