81dae3efb9e797b580e0860bb0117fbd4517531d
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-06-07 03:03:40 +0545

    section: Modify hero section for scroll issue.

--- components/HeroHome.tsx
+++ components/HeroHome.tsx
@@ -1,23 +1,84 @@
-import React from 'react'
-import Image from 'next/image'
-import { Oswald } from 'next/font/google'
-
-const oswald = Oswald({subsets: ['latin'], weight: ['400', '200', '300', '500']})
+import React from 'react';
+import Image from 'next/image';
+import { oswald } from '@/constants';
 
 const HeroHome = () => {
   return (
-    <div className={`bg-[#4eceff] h-[180vh] max-mini:h-[210vh] w-[100vw] ${oswald.className} flex flex-col justify-center items-center`}>
-        <Image src='/goSaiyan.webp' width={1100} height={500} alt='logo' className='absolute top-[22%] left-[18vw] w-[64%] max-pad:top-[13%] max-mini:top-[18%] max-sm:w-[22rem] max-sm:left-8'></Image>
-        <p className='text-white w-[30%] text-center -mt-[60vh] max-mini:-mt-[80rem] max-pad:-mt-[94rem] max-air:-mt-[85rem] max-sm:w-[95%] max-sm:-mt-[70rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam beatae sit doloribus atque</p>
-        <button className='bg-[#fff500] text-[#4eceff] py-[0.8rem] px-[1.4rem] rounded-md font-bold text-[20px] mt-[1.5rem]'>SHOP NOW</button>
-        <Image src='/mainCan.webp' alt='main can' width={2300} height={0} className='absolute top-[75%] left-[12%] max-pad:top-[40%] max-mini:w-[88%] max-mini:top-[55%] max-sm:w-[370px]'></Image>
-        <Image src='/redOrange.webp' alt='red peach' height={200} width={200} className='absolute top-[40%] left-[0%] max-pad:top-[10%] max-pad:-left-[5%] max-mini:w-[20%]'></Image>
-        <Image src='/mainPeach.webp' alt='main peachs' height={200} width={150} className='max-mini:w-[15%] max-pad:top-[8%] max-pad:-right-[2.5%] absolute top-[20%] right-[1vw]'></Image>
-        <Image src='/mainLime.webp' alt='main lime' height={200} width={150} className='max-mini:w-[18%] absolute top-[80%] right-[2%] max-pad:top-[40%]'></Image>
-        <Image src='/leftLeaf.webp' alt='main leaf' height={200} width={250} className='max-mini:w-[22%] absolute top-[110%] -left-[3%] rotate-45 max-pad:top-[70%]'></Image>
-        <Image src='/rightLeafe.webp' alt='main leaf' height={200} width={220} className='absolute top-[150%] -right-[5%] -rotate-12 max-pad:hidden'></Image>
-    </div>
-  )
-}
+    <div
+      className={`bg-[#4eceff] min-h-screen w-full ${oswald.className} flex flex-col justify-center items-center relative overflow-x-hidden`}
+    >
+      {/* Logo Image */}
+      <Image
+        src="/goSaiyan.webp"
+        width={1100}
+        height={500}
+        alt="logo"
+        className="relative w-3/4 max-w-[64%] mt-40 max-sm:w-[90%] max-sm:mt-10"
+        priority
+      />
 
-export default HeroHome
+      {/* Text */}
+      <p
+        className="text-white w-1/3 text-center mt-3 max-sm:w-[90%] max-sm:mt-4"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam beatae sit doloribus atque
+      </p>
+
+      {/* Button */}
+      <button
+        className="bg-[#fff500] text-[#4eceff] py-3 px-6 rounded-md font-bold text-xl mt-6 hover:bg-[#e0d000]"
+      >
+        SHOP NOW
+      </button>
+
+      {/* Main Can Image */}
+      <Image
+        src="/mainCan.webp"
+        alt="main can"
+        width={2300}
+        height={0}
+        className="relative w-3/4 max-w-[80%] mt-16 max-sm:w-[90%]"
+        priority
+      />
+
+      {/* Decorative Images */}
+      <Image
+        src="/redOrange.webp"
+        alt="red peach"
+        height={200}
+        width={200}
+        className="absolute top-[15%] left-[5%] w-[15%] max-sm:w-[20%]"
+      />
+      <Image
+        src="/mainPeach.webp"
+        alt="main peach"
+        height={200}
+        width={150}
+        className="absolute top-[10%] right-[5%] w-[10%] max-sm:w-[15%]"
+      />
+      <Image
+        src="/mainLime.webp"
+        alt="main lime"
+        height={200}
+        width={150}
+        className="absolute top-[60%] right-[5%] w-[12%] max-sm:w-[18%]"
+      />
+      <Image
+        src="/leftLeaf.webp"
+        alt="main leaf"
+        height={200}
+        width={250}
+        className="absolute top-[70%] -left-[2%] w-[15%] rotate-45 max-sm:w-[20%]"
+      />
+      <Image
+        src="/rightLeafe.webp"
+        alt="main leaf"
+        height={200}
+        width={220}
+        className="absolute top-[80%] -right-[2%] w-[15%] -rotate-12 max-sm:hidden"
+      />
+    </div>
+  );
+};
+
+export default HeroHome;
